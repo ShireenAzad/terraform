3be37a6de23fb007821bc2a852bb633a1200b540
ShireenAzad <syed.shireen@everest.engineering>
2022-05-25 23:35:16 +0530

    cross origins added.

--- main.tf
+++ main.tf
@@ -55,6 +55,18 @@ resource "aws_security_group" "security_port" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # outbound from jenkis server
   egress {
