d173de704b3e179cc02ab8d14f5e35d2e217151e
ShireenAzad <syed.shireen@everest.engineering>
2022-05-26 11:30:37 +0530

    cross origins added.

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "aws_instance" "employeePortal" {
   instance_type = var.instance_type
   subnet_id     = aws_subnet.prod-subnet-public-1.id
 
-  vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+  vpc_security_group_ids = ["${aws_security_group.db.id}"]
 
   tags = {
     Name = "employeeportal_ec2_instance"
