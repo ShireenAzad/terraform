d59439d72b4835fefb06575177e4067fc6fad410
ShireenAzad <syed.shireen@everest.engineering>
2022-05-26 08:02:01 +0530

    cross origins added.

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/* terraform {
   backend "s3"{
     region     = "ap-south-1"
     bucket     = "employeeportalbucket"
     key        = "terraform.tfstate"
     encrypt    = "false"
   }
-}
+} */
